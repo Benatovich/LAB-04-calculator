bccfed434faa1b8c5b76dae8c12500b6564bb2f4
Benatovich <sambenatovich@gmail.com>
2021-10-07 17:27:33 -0400

    add function working properly

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 // import functions
-import {add} from './calculations.js';
-console.log(add(50, 212));
+import { add } from './calculations.js';
+//console.log(add(50, 212));
+
 
 // reference needed DOM elements
 const addNumber1 = document.getElementById('add-number-1');
@@ -13,3 +14,11 @@ const addAnswer = document.getElementById('add-answer');
     // get user input(s)
     // do any needed work with the value(s)
     // update DOM to reflect new value(s)
+addButton.addEventListener('click', () => {
+    //this code runs when the add button is clicked
+    // console.log(add(Number(addNumber1.value), Number(addNumber2.value)));
+    const sum = add(Number(addNumber1.value), Number(addNumber2.value));
+    console.log(sum);
+    addAnswer.textContent = sum;
+});
+
