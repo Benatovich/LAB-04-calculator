f7ee5129a9df072ed1ac414e03f70b604dd13090
Benatovich <sambenatovich@gmail.com>
2021-10-07 18:16:55 -0400

    addition, subtraction, multiplication, and division work

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 // import functions
 import { add } from './calculations.js';
 //console.log(add(50, 212));
+import { subtract } from './calculations.js';
+import { multiply } from './calculations.js';
+import { divide } from './calculations.js';
+
 
 
 // reference needed DOM elements
@@ -8,6 +12,21 @@ const addNumber1 = document.getElementById('add-number-1');
 const addNumber2 = document.getElementById('add-number-2');
 const addButton = document.getElementById('add-button');
 const addAnswer = document.getElementById('add-answer');
+
+const subtractNumber1 = document.getElementById('subtract-number-1');
+const subtractNumber2 = document.getElementById('subtract-number-2');
+const subtractButton = document.getElementById('subtract-button');
+const subtractAnswer = document.getElementById('subtract-answer');
+
+const multiplyNumber1 = document.getElementById('multiply-number-1');
+const multiplyNumber2 = document.getElementById('multiply-number-2');
+const multiplyButton = document.getElementById('multiply-button');
+const multiplyAnswer = document.getElementById('multiply-answer');
+
+const divideNumber1 = document.getElementById('divide-number-1');
+const divideNumber2 = document.getElementById('divide-number-2');
+const divideButton = document.getElementById('divide-button');
+const divideAnswer = document.getElementById('divide-answer');
 
 
 // set event listeners 
@@ -20,5 +39,23 @@ addButton.addEventListener('click', () => {
     const sum = add(Number(addNumber1.value), Number(addNumber2.value));
     console.log(sum);
     addAnswer.textContent = sum;
+});
+
+subtractButton.addEventListener('click', () => {
+    const difference = subtract(Number(subtractNumber1.value), Number(subtractNumber2.value));
+    console.log(difference);
+    subtractAnswer.textContent = difference;
+});
+
+multiplyButton.addEventListener('click', () => {
+    const product = multiply(Number(multiplyNumber1.value), Number(multiplyNumber2.value));
+    console.log(product);
+    multiplyAnswer.textContent = product;
+});
+
+divideButton.addEventListener('click', () => {
+    const quotient = divide(Number(divideNumber1.value), Number(divideNumber2.value));
+    console.log(quotient);
+    divideAnswer.textContent = quotient;
 });
 
